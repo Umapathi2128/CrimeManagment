c8ed659dfa9ea7a5e075b28bf6808c8acef82ced
Umapathi2128 <33646765+Umapathi2128@users.noreply.github.com>
2018-04-24 16:28:42 +0530

    second commit

---
+++ app/src/main/java/com/example/yugan/abc/ui/reports/user/UserDataModel.kt
@@ -0,0 +1,3 @@
+package com.example.yugan.abc.ui.reports.user
+
+class UserDataModel(var complaint:String,var status:String,var ratePolice:String,var complaintAgainstPolice:String,var logout:String)
